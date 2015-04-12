b990323337d95adab8647881a4b341d56e345e5e
srph <seraphipod@gmail.com>
2015-04-13 06:55:11 +0800

    Added upper-case-first.

--- src/components/Product/ProductHeader.jsx
+++ src/components/Product/ProductHeader.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import uppercase from 'upper-case-first';
 
 export default React.createClass({
   render() {
     var { filter } = this.props;
     var uppercased = !filter.length
       ? 'All'
-      : filter.charAt(0).toUpperCase() + filter.slice(1, filter.length);
+      : uppercase(filter);
 
     return (
       <h1>
