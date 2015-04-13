eabe71119e0a6a92391f95a8ae4a00815774fbcc
srph <seraphipod@gmail.com>
2015-04-13 12:13:15 +0800

    Added Github Ribbon.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { RouteHandler } from 'react-router';
+import GithubRibbon from './GithubRibbon';
 import CategoryLinks from './CategoryLinks/CategoryLinks';
 import Cart from './Cart/Cart';
 
 export default React.createClass({
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8">
-            <CategoryLinks />
-            <RouteHandler />
-          </div>
+      <div>
+        <GithubRibbon />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8">
+              <CategoryLinks />
+              <RouteHandler />
+            </div>
 
-          <div className="col-md-4">
-            <Cart />
+            <div className="col-md-4">
+              <Cart />
+            </div>
           </div>
         </div>
       </div>
