6649f3a15bcb7aeca913ceee597772921db765b9
srph <seraphipod@gmail.com>
2015-04-17 10:07:55 +0800

    Added price indicator for each product. Resolves https://github.com/srph/jumars-tindahan/issues/1.

--- src/components/Product/ProductItem.jsx
+++ src/components/Product/ProductItem.jsx
@@ -5,13 +5,18 @@ export default React.createClass({
   render() {
     var { data } = this.props;
     var id = data.get('id');
+    var name = data.get('name');
     var stock = data.get('stock');
     var img = data.get('img');
+    var price = data.get('price');
+    var remaining = data.get('stock') ? ' remaining' : '';
+    
     return (
       <div className="item">
         <div className="body">
           <div className="thumbnail"> <img src={`assets/img/${img}`} /> </div>
-          <span>{data.get('name')} ({stock})</span>
+          <h3>{name} ({stock}{remaining})</h3>
+          <h5>{price}</h5>
           <ProductItemBuyButton stock={!!stock} id={id} />
         </div>
       </div>
